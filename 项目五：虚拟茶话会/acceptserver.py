0dc00e1cfebe4e255fd51f67b63bf0216c9557b2
yuzhipeng <yuzp1996@qq.com>
2017-03-06 13:18:53 +0800

    茶话会

--- 项目五：虚拟茶话会/acceptserver.py
+++ 项目五：虚拟茶话会/acceptserver.py
@@ -4,11 +4,11 @@ import socket, asyncore
 class ChatServer(dispatcher):
 
     def handle_accept(self):
-        conn, addr = self.accept()
+        conn, addr = self.accept() # return a connection and a address
         print 'Connection atttempt from ', addr[0]
 
 s = ChatServer()
-s.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+s.create_socket(socket.AF_INET, socket.SOCK_STREAM) #identify the type of the socket 
 s.bind(('', 5005))
 s.listen(6)
-asyncore.loop()
+asyncore.loop() #loop
